693d755644a8dc9a28756c07971bcd5b429d5a5e
Lakshay010-1 <black.leopard0987>
2023-11-26 20:13:05 +0530

    Added local repo

---
+++ Projects/project01.java
@@ -0,0 +1,31 @@
+package com.chetancodes;
+import java.util.Scanner;
+
+// this percentage calculator is for 5 subjects
+
+public class project01 {
+    public static void main(String[] args) {
+
+        System.out.println("enter your subjects marks");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your subject01 marks");
+        int a = sc.nextInt();
+        System.out.println("Enter your subject02 marks");
+        int b = sc.nextInt();
+        System.out.println("Enter your subject03 marks");
+        int c = sc.nextInt();
+        System.out.println("Enter your subject04 marks");
+        int d = sc.nextInt();
+        System.out.println("Enter your subject05 marks");
+        int e = sc.nextInt();
+        System.out.println("marks are out of");
+        int f = sc.nextInt();
+
+        int g = a+b+c+d+e ;
+        int h = 5 * f ;
+        float m = g*100.0f/ h;
+        System.out.println("your % is "+ m);
+
+
+ }
+}
---
+++ Projects/project03.java
@@ -0,0 +1,57 @@
+package com.chetancodes;
+import java.util.*;
+// guess the number game with computer
+public class project03{
+    public static void main(String[] args) {
+        guessTheNum a=new guessTheNum();
+        a.ComputerInput();
+        a.UserInput();
+        a.isCorrectNum();
+ }
+}
+class guessTheNum{ 
+    int userInput,computerInput;
+    private int noOfGuesses;
+
+    public void setNoOfGuesses(){
+        noOfGuesses++;
+    }
+    public void getNoOfGuesses(){
+        System.out.println("You took "+noOfGuesses+" guesses.");
+    }
+    public void ComputerInput(){
+        Random rd=new Random();
+        computerInput= rd.nextInt(100);
+        System.out.println("computer had chosen its number ");
+        //return computerInput;
+    }
+    public void UserInput(){
+        System.out.println("enter your name between 1 to 100");
+        Scanner sc=new Scanner(System.in);
+        userInput=sc.nextInt(100);
+        //return userInput;
+
+    }
+    public void isCorrectNum(){
+        if(userInput==computerInput){
+            setNoOfGuesses();
+            System.out.println("you guessed the number right "+userInput);
+            getNoOfGuesses();
+        }
+        else if(userInput>computerInput){
+            setNoOfGuesses();
+            System.out.println("Choose a smaller number than "+userInput);
+            System.out.println();
+            UserInput();
+            isCorrectNum();
+        }
+    else{ setNoOfGuesses();
+            System.out.println("Choose a larger number  than "+userInput);
+            System.out.println();
+            UserInput();
+            isCorrectNum();
+        }}
+}
+
+
+
