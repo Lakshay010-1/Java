d49016090606702824e156d8108eac0490c59127
Lakshay010-1 <black.leopard0987@gmail.com>
2024-02-16 05:47:17 +0530

    PriorityQueue basics and questions.

--- PriorityQueue_Heaps/PriorityQueue_01.java
+++ PriorityQueue_Heaps/PriorityQueue_01.java
@@ -3,22 +3,7 @@ import java.util.PriorityQueue;
 import java.util.Arrays;
 
 public class PriorityQueue_01 {
-    public static int[] kthLargest(int k, int[] arr, int n) {
-        // code here
-        if(k<=0){
-            return new int[]{};
-        }
-        int ans[]=new int[n];
-        
-        for(int i=0;i<k-1;i++){
-            ans[i]=-1;
-        }
-        for(int i=k-1;i<n;i++){
-            Arrays.sort(arr,0,i+1);
-            ans[i]=arr[k-i-1];
-        }
-        return ans;
-    }
+    
     static class Coordinates implements Comparable<Coordinates>{
         int idx,distance,x,y;
 
@@ -49,20 +34,24 @@ public class PriorityQueue_01 {
             return this.soldr-r.soldr;
         }
     }
-
-    static class Block implements Comparable<Block>{
-        int value,idx;
-
-        Block(int value,int i){
-            this.value=value;
-            this.idx=i;
+    public static int[] kthLargest(int k, int[] arr, int n) {
+        // code here
+        if(k<=0){
+            return new int[]{};
         }
-
-        public int compareTo(Block b){
-            return b.value-this.value;
+        int ans[]=new int[n];
+        
+        for(int i=0;i<k-1;i++){
+            ans[i]=-1;
         }
+        for(int i=k-1;i<n;i++){
+            Arrays.sort(arr,0,i+1);
+            ans[i]=arr[k-i-1];
+        }
+        return ans;
     }
 
+    
     public static void main(String[] args) {
 
         //Nearest coordinate close to coordinate(0,0);
@@ -106,28 +95,6 @@ public class PriorityQueue_01 {
             System.out.println("row"+pq3.remove().idx);
         }
 
-        //Sliding Window maximum
-        int arr[]={1,3,-1,-3,5,3,6,7};
-        k=3;
-        int ans[]=new int[arr.length-k+1];
-        PriorityQueue<Block> pq4=new PriorityQueue<>();
-
-        for(int i=0;i<k;i++){
-            pq4.add(new Block(arr[i], i));
-        }
-        ans[0]=pq4.peek().value;
-
-        for(int i=k;i<arr.length;i++){
-            while(!pq4.isEmpty() && pq4.peek().idx<=(i-k)){
-                pq4.remove();
-            }
-            pq4.add(new Block(arr[i], i));
-            ans[i-k+1]=pq4.peek().value;
-        }     
-        for(int i=0;i<ans.length;i++){
-            System.out.print(ans[i]+" ");
-        }
-
-
+        
     }
 }
