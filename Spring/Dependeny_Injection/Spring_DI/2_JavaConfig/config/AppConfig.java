6cad26e15b391c2fbc30e960db239183012e053f
Lakshay <black.leopard0987@gmail.com>
2024-06-26 16:40:24 +0530

    DI Using Java Based Config

---
+++ Spring/Dependeny_Injection/Spring_DI/2_JavaConfig/config/AppConfig.java
@@ -0,0 +1,35 @@
+package org.example.config;
+import org.example.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+//    @Bean(name={"com1","desk","top",})
+//    @Bean(name="Com1")
+    @Bean
+    @Scope("prototype")
+    public Desktop desktop(){
+        return new Desktop();
+    }
+    @Bean
+    public Alien alien( @Autowired Computer com){ //public Alien alien( @Qualifier("laptop") @Autowired Computer com ){ 
+        Alien obj=new Alien();
+        obj.setLanguage("rust");
+        obj.setCom(com);
+        return obj;
+    }
+    @Bean
+    public Laptop laptop(){
+        return new Laptop();
+    }
+    @Bean
+    @Primary
+    public SmartGlass smartGlass(){
+        return new SmartGlass();
+    }
+}
