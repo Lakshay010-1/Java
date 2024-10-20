e9c7a100b541b7e4da30f7524cc1314304440b89
Lakshay <black.leopard0987@gmail.com>
2024-10-20 13:06:19 +0530

    Spring Data Rest

---
+++ Spring/Rest_SpringBoot/src/main/java/org/program/data_Rest/RestSpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package org.program.data_Rest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringDataRestApplication.class, args);
+		System.out.println("JobPortal using Spring Data Rest (backend)");
+	}
+
+}
