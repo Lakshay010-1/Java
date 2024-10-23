aeabf258e865360f6975860f6f23b77dfd7f830d
Lakshay <black.leopard0987@gmail.com>
2024-10-23 18:51:57 +0530

    Job Portal Backend using Spring Rest

--- Spring/Rest_SpringBoot_JobPortal/src/main/java/org/program/Rest/model/JobPost.java
+++ Spring/Rest_SpringBoot_JobPortal/src/main/java/org/program/Rest/model/JobPost.java
@@ -14,7 +14,7 @@ public class JobPost {
     @Id
     private int postId;
     private String postProfile;
-    private String postDesc;
+    private String postDesc; 
     private int reqExperience;
     private List<String> postTechStack;
 
